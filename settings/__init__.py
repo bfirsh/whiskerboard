7df02ed91bbff9f60c841626bf203a47ead15d43
Alexis Métaireau <alexis@notmyidea.org>
2012-12-29 01:02:44 +0100

    use the settings/deploy.py if it exists

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import
 from django.core.exceptions import ImproperlyConfigured
-import os
 
 try:
-    from .local import *
+    from .deploy import *
 except ImportError:
-    raise ImproperlyConfigured('You need to create settings/local.py and set SECRET_KEY. If you deploy with Fabric, it will automatically create this for you.')
-
+    try:
+        from .local import *
+    except ImportError:
+        raise ImproperlyConfigured('You need to create settings/local.py and set SECRET_KEY. If you deploy with Fabric, it will automatically create this for you.')
